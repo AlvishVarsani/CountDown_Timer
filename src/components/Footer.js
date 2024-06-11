ac6d50d07008fbeda6398a85c0f7bf8673365cb9
alvish varsani <alvishvarsani2003@gmail.com>
2024-06-11 21:36:49 +0530

    add the header, footer,countdown components

---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Footer = () => {
+  return <footer>Created by <a href="" target="_blank">Alvish Varsani</a> &copy; {new Date().getFullYear()}</footer>;
+}
+
+export default Footer;
